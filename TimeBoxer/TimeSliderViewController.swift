78cb2b2112869cafd0f3e0e314852547debbc8af
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2016-01-02 20:38:50 +0100

    Completed the timeSliderVC to timerRunningVC transition. Wired and working

--- TimeBoxer/TimeSliderViewController.swift
+++ TimeBoxer/TimeSliderViewController.swift
@@ -15,8 +15,7 @@ class TimeSliderViewController: UIViewController {
     
     let startButtonBackgroundColor = UIColor(red:1.0, green:0.945, blue: 0.902, alpha:1.0)
     let startButtonFrontLayerColor = UIColor(white:0.15, alpha:1.0)
-    
-    private let transitionManager = TransitionManager(animator: CustomAnimator(), dismissAnimator: nil)
+    private let toTimerRunningVCAnimator = ToTimerRunningAnimator()
 
 //----------------------------------------------------------------------------------------------------------------------
     override func viewDidLoad() {
@@ -27,14 +26,12 @@ class TimeSliderViewController: UIViewController {
         startButton.ovalLayerHighlightedColor = startButtonBackgroundColor
         startButton.borderColor = startButtonFrontLayerColor
         startButton.borderWidth = 1.0
-        // Do any additional setup after loading the view.
 
     }
     
 //----------------------------------------------------------------------------------------------------------------------
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -62,14 +59,13 @@ class TimeSliderViewController: UIViewController {
         
         let timerRunningViewController = segue.destinationViewController as! TimerRunningViewController
         timerRunningViewController.counter = timeSlider.value * 60
-        timerRunningViewController.transitioningDelegate = transitionManager
+        
     }
 
 //----------------------------------------------------------------------------------------------------------------------
-    @IBAction func unwindToTimeSliderVC(sender: UIStoryboardSegue) {
-        if let segueIdentifier = sender.identifier {
-            print("unwind \(segueIdentifier)")
-        }
+    override func showViewController(vc: UIViewController, sender: AnyObject?) {
+        let containerVC = parentViewController as! ContainerViewController
+        containerVC.switchViewControllers(self, toVC: vc, animator: toTimerRunningVCAnimator)
     }
 }
 
@@ -169,103 +165,6 @@ private class ToTimerRunningAnimator:NSObject, Animator {
         let animation = CABasicAnimation(keyPath: "opacity")
         animation.delegate = self
         animation.duration = transitionDuration / 2.0
-        animation.fromValue = 0.0
-        animation.toValue = 1.0
-        timerRunningLayer.addAnimation(animation, forKey: "opacity")
-        return timerRunningLayer
-    }
-    
-}
-
-//
-// MARK: - CustomAnimator
-//
-
-private class CustomAnimator: NSObject, UIViewControllerAnimatedTransitioning {
-    var context:UIViewControllerContextTransitioning?
-    var container: UIView?
-    var timeSliderVC: TimeSliderViewController?
-    var timerRunningVC: TimerRunningViewController?
-    var animationCounter = 0
-    var animationLayers = [CALayer]()
-    
-//----------------------------------------------------------------------------------------------------------------------
-    @objc func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval
-    {
-        return 0.5
-    }
-    
-//----------------------------------------------------------------------------------------------------------------------
-    @objc func animateTransition(transitionContext: UIViewControllerContextTransitioning)
-    {
-        animationCounter = 0
-        self.context = transitionContext
-        self.container = transitionContext.containerView()
-        self.timeSliderVC = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)
-            as? TimeSliderViewController
-        self.timerRunningVC = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)
-            as? TimerRunningViewController
-        let circleLayer = prepareExpandingCircleAnimationLayer()
-        animationLayers.append(circleLayer)
-        container!.layer.addSublayer(circleLayer)
-    }
-    
-//----------------------------------------------------------------------------------------------------------------------
-    override func animationDidStop(anim: CAAnimation, finished flag: Bool)
-    {
-        if animationCounter == 0 {
-            animationCounter++
-            prepareFadeInAnimationLayer()
-            container!.addSubview(timerRunningVC!.view)
-        } else {
-            timeSliderVC!.view.removeFromSuperview()
-            for animationLayer in animationLayers {
-                animationLayer.removeFromSuperlayer()
-            }
-            context!.completeTransition(true)
-        }
-    }
-    
-//----------------------------------------------------------------------------------------------------------------------
-    private func prepareExpandingCircleAnimationLayer() -> CALayer
-    {
-        
-        let startButton = timeSliderVC!.startButton
-        let circleCenter:CGPoint = container!.convertPoint(startButton.center, fromView: startButton.superview)
-        let smallCircle = CirclePathWrapper(centerX: circleCenter.x, centerY: circleCenter.y, radius: startButton.radius)
-        let largeCircle = createLargeCircleForButton(startButton)
-        let shapeLayer = CAShapeLayer()
-        shapeLayer.fillColor = timerRunningVC!.view.backgroundColor!.CGColor
-        shapeLayer.lineWidth = 0.0
-        shapeLayer.path = largeCircle.path
-        let animation = CABasicAnimation(keyPath: "path")
-        animation.delegate = self
-        animation.duration = transitionDuration(context!) / 2.0
-        animation.fromValue = smallCircle.path
-        animation.toValue = largeCircle.path
-        shapeLayer.addAnimation(animation, forKey: "path")
-        return shapeLayer
-    }
-    
-//----------------------------------------------------------------------------------------------------------------------
-    private func createLargeCircleForButton(button:AbstractOvalButton) -> CirclePathWrapper
-    {
-        let circleCenter:CGPoint = container!.convertPoint(button.center, fromView: button.superview)
-        let xs = circleCenter.x
-        let ys = circleCenter.y
-        let x0 = container!.frame.origin.x
-        let y0 = container!.frame.origin.y
-        let  largeRadius = sqrt(pow((xs - x0),2) + pow((ys - y0),2))
-        return CirclePathWrapper(centerX: xs, centerY: ys, radius: largeRadius)
-    }
-
-//----------------------------------------------------------------------------------------------------------------------
-    private func prepareFadeInAnimationLayer() -> CALayer
-    {
-        let timerRunningLayer = timerRunningVC!.view.layer
-        let animation = CABasicAnimation(keyPath: "opacity")
-        animation.delegate = self
-        animation.duration = transitionDuration(context!) / 2.0
         animation.fromValue = 0.0
         animation.toValue = 1.0
         timerRunningLayer.addAnimation(animation, forKey: "opacity")
