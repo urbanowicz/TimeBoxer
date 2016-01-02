e4696a66242c7e6b898cd1145f18a4a789506aa7
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2016-01-02 17:45:56 +0100

    create the ContainerViewController

---
+++ TimeBoxer/ContainerViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ContainerViewController.swift
+//  TimeBoxer
+//
+//  Created by Tomasz Urbanowicz on 02.01.2016.
+//  Copyright © 2016 Tomasz Urbanowicz. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+    
+    var timeSliderVC:TimeSliderViewController?
+    var tmerRunningVC:TimerRunningViewController?
+    var timerPausedVC:TimerPausedViewController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
