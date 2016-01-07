46940fa79df3a799983012f481d0dee7dccd3f5a
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2016-01-07 18:30:48 +0100

    Created an empty EditProjectViewController

---
+++ TimeBoxer/EditProjectViewController.swift
@@ -0,0 +1,23 @@
+//
+//  EditProjectViewController.swift
+//  TimeBoxer
+//
+//  Created by Tomasz Urbanowicz on 07.01.2016.
+//  Copyright © 2016 Tomasz Urbanowicz. All rights reserved.
+//
+
+import UIKit
+
+class EditProjectViewController: UIViewController {
+
+//----------------------------------------------------------------------------------------------------------------------
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
