c979e45642958e2db03f178c13eb826a6bc69341
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2016-01-07 18:33:49 +0100

    Created an empty AddProjectViewController

---
+++ TimeBoxer/AddProjectViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AddProjectViewController.swift
+//  TimeBoxer
+//
+//  Created by Tomasz Urbanowicz on 07.01.2016.
+//  Copyright © 2016 Tomasz Urbanowicz. All rights reserved.
+//
+
+import UIKit
+
+class AddProjectViewController: UIViewController {
+
+//----------------------------------------------------------------------------------------------------------------------
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
