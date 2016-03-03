6135ffe122a2199f6515d529c0d74efb0d054b1a
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2016-03-03 19:21:26 +0100

    Work on TimerDoneVC #1

--- TimeBoxer/TimerDoneViewController.swift
+++ TimeBoxer/TimerDoneViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class TimerDoneViewController: UIViewController {
 
+    @IBOutlet weak var appTitleLabel: UILabel!
+    @IBOutlet weak var completedLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var ofLabel: UILabel!
+    @IBOutlet weak var projectName: UILabel!
+    
     @IBOutlet weak var okButton: OKButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = Colors.seafoam()
+        setupAppTitleLabel()
+        setupCompletedLabel()
+        setupTimeLabel()
+        setupOfLabel()
         setupOKButton()
+        setupProjectNameLabel()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +34,23 @@ class TimerDoneViewController: UIViewController {
     }
 
 //MARK: Setup UI elements
+    private func setupAppTitleLabel() {
+        appTitleLabel.textColor = Colors.almostBlack()
+        appTitleLabel.sizeToFit()
+    }
+    private func setupCompletedLabel() {
+        completedLabel.textColor = Colors.lightGray()
+    }
+    private func setupTimeLabel() {
+        timeLabel.textColor = Colors.almostBlack()
+    }
+    private func setupOfLabel() {
+        ofLabel.textColor = Colors.lightGray()
+    }
+    private func setupProjectNameLabel() {
+        projectName.textColor = Colors.almostBlack()
+    }
+    
     private func setupOKButton() {
         okButton.borderColor = Colors.almostBlack()
         okButton.ovalLayerColor = Colors.seafoam()
