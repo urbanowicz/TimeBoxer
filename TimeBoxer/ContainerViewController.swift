9b31f12a8d4fc763a1c9157a96fa9debd2b277ae
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2016-03-04 21:18:35 +0100

    Work on Projects Table to Time Slider swipe and vice versa  (Almost done)

--- TimeBoxer/ContainerViewController.swift
+++ TimeBoxer/ContainerViewController.swift
@@ -13,6 +13,7 @@ class ContainerViewController: UIViewController, UIGestureRecognizerDelegate{
     private var projectsTableVC: ProjectsTableViewController?
         
     private var toTimeSliderSwipeHandler:ProjectsTableToTimeSliderSwipeHandler?
+    private var toProjectsTableSwipeHandler:TimeSliderToProjectsTableSwipeHandler?
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -81,46 +82,44 @@ class ContainerViewController: UIViewController, UIGestureRecognizerDelegate{
 
     }
 
-override func prefersStatusBarHidden() -> Bool {
-    return true;
-}
+    override func prefersStatusBarHidden() -> Bool {
+        return true;
+    }
 
 
 //MARK: PanGestureRecognizer
     func handlePanGesture(gestureRecognizer: UIPanGestureRecognizer) {
         let currentVC = childViewControllers[0] as? TimeSliderViewController
         
-        var fromVC: UIViewController
         var toVC: UIViewController
         var direction:Int // direction == 1 is right to left, direction == -1 is left to right
         
         if currentVC != nil {
             timeSliderVC = currentVC
-            fromVC = currentVC!
             toVC = projectsTableVC!
             direction = 1
         } else {
-            fromVC = projectsTableVC!
             toVC = timeSliderVC!
             direction = -1
         }
         
         if gestureRecognizer.state == .Began {
-            if direction == -1 {
-                toTimeSliderSwipeBegan(gestureRecognizer)
-            }
             toVC.view.frame = self.view.frame
             toVC.view.frame.origin.x  = self.view.frame.origin.x - view.frame.width * CGFloat(direction)
             self.view.addSubview(toVC.view)
+            if direction == -1 {
+                toTimeSliderSwipeBegan(gestureRecognizer)
+            } else {
+                self.toProjectsTableSwipeHandler =
+                    TimeSliderToProjectsTableSwipeHandler(fromVC: timeSliderVC!, toVC: projectsTableVC!, containerVC: self)
+            }
         }
         
         if gestureRecognizer.state == .Changed {
             if direction == -1 {
                 toTimeSliderSwipeHandler!.handleSwipeChanged(gestureRecognizer)
             } else {
-                let translation = gestureRecognizer.translationInView(view)
-                fromVC.view.frame.origin.x = view.frame.origin.x + translation.x
-                toVC.view.frame.origin.x = (view.frame.origin.x + translation.x) - view.frame.width * CGFloat(direction)
+                toProjectsTableSwipeHandler!.handleSwipeChanged(gestureRecognizer)
             }
         }
         
@@ -128,43 +127,8 @@ override func prefersStatusBarHidden() -> Bool {
             if direction == -1 {
                 toTimeSliderSwipeHandler!.handleSwipeEnded(gestureRecognizer)
                 return
-            }
-            let translation = gestureRecognizer.translationInView(view)
-            if fabs(translation.x) > view.frame.width / 2.0 {
-                
-                UIView.animateWithDuration(0.1,
-                    animations:
-                    {
-                        toVC.view.frame.origin.x = self.view.frame.origin.x
-                        fromVC.view.frame.origin.x = self.view.frame.origin.x + self.view.frame.width * CGFloat(direction)
-                        
-                    },
-                    
-                    completion:
-                    {
-                        finished in
-                        self.addChildViewController(toVC)
-                        toVC.didMoveToParentViewController(self)
-                        fromVC.willMoveToParentViewController(nil)
-                        fromVC.view.removeFromSuperview()
-                        fromVC.removeFromParentViewController()
-                        
-
-                    })
             } else {
-                UIView.animateWithDuration(0.1,
-                    animations:
-                    {
-                        fromVC.view.frame.origin.x = self.view.frame.origin.x
-                        toVC.view.frame.origin.x = self.view.frame.origin.x - self.view.frame.width * CGFloat(direction)
-                    },
-                    
-                    completion:
-                    {
-                        finished in
-                        toVC.view.removeFromSuperview()
-                    }
-                )
+                toProjectsTableSwipeHandler!.handleSwipeEnded(gestureRecognizer)
             }
         }
     }
@@ -201,9 +165,13 @@ override func prefersStatusBarHidden() -> Bool {
     }
 }
 
+
+
+//
+//MARK: PtojectsTableToTimeSliderSwipeHandler
+//
 private class ProjectsTableToTimeSliderSwipeHandler: NSObject {
     var tableCell: UITableViewCell
-    //var tableView: UIView
     var fromView:UIView
     var toView: UIView
     var containerView: UIView
@@ -217,7 +185,6 @@ private class ProjectsTableToTimeSliderSwipeHandler: NSObject {
     init(tableCell:UITableViewCell, fromVC: UIViewController, toVC: UIViewController, containerVC: ContainerViewController) {
         self.tableCell = tableCell
         self.tableCellOrigin = tableCell.frame.origin
-        //self.tableView = projectsTableVC.projectsTableView
         self.fromView = fromVC.view
         self.fromVC = fromVC
         self.toView = toVC.view
@@ -235,7 +202,6 @@ private class ProjectsTableToTimeSliderSwipeHandler: NSObject {
                 //Don't let the user drag the cell in the opposite direction now
                 return
             }
-            print(translationInProjectsTableView.x)
             tableCell.frame.origin.x = tableCellOrigin.x + translationInProjectsTableView.x
         } else {
             let translation = gestureRecognizer.translationInView(containerView)
@@ -308,5 +274,76 @@ private class ProjectsTableToTimeSliderSwipeHandler: NSObject {
                 self.tableCell.frame.origin.x = self.tableCellOrigin.x
                 
         })
+    }
+}
+
+private class TimeSliderToProjectsTableSwipeHandler: NSObject {
+    var fromView:UIView
+    var toView: UIView
+    var containerView: UIView
+    
+    var fromVC: UIViewController
+    var toVC: UIViewController
+    var containerVC: ContainerViewController
+    
+    init(fromVC: UIViewController, toVC: UIViewController, containerVC: ContainerViewController) {
+        self.fromVC = fromVC
+        self.toVC = toVC
+        self.containerVC = containerVC
+        self.fromView = fromVC.view
+        self.toView = toVC.view
+        self.containerView = containerVC.view
+    }
+    
+    func handleSwipeChanged(gestureRecognizer:UIPanGestureRecognizer) {
+        let translation = gestureRecognizer.translationInView(containerView)
+        fromView.frame.origin.x = containerView.frame.origin.x + translation.x
+        toView.frame.origin.x = (containerView.frame.origin.x + translation.x) - containerView.frame.width
+    }
+    
+    func handleSwipeEnded(gestureRecognizer:UIPanGestureRecognizer) {
+        let translation = gestureRecognizer.translationInView(containerView)
+        if translation.x >= containerView.frame.width / 2.0 {
+            commitTransition()
+        } else {
+            rollbackTransition()
+        }
+    }
+    
+    func commitTransition() {
+        UIView.animateWithDuration(0.1,
+            animations:
+            {
+                self.toView.frame.origin.x = self.containerView.frame.origin.x
+                self.fromView.frame.origin.x = self.containerView.frame.origin.x + self.containerView.frame.width
+                
+            },
+            
+            completion:
+            {
+                finished in
+                self.containerVC.addChildViewController(self.toVC)
+                self.toVC.didMoveToParentViewController(self.containerVC)
+                self.fromVC.willMoveToParentViewController(nil)
+                self.fromVC.view.removeFromSuperview()
+                self.fromVC.removeFromParentViewController()
+                
+        })
+    }
+    
+    func rollbackTransition() {
+        UIView.animateWithDuration(0.1,
+            animations:
+            {
+                self.fromView.frame.origin.x = self.containerView.frame.origin.x
+                self.toView.frame.origin.x = self.containerView.frame.origin.x - self.containerView.frame.width
+            },
+            
+            completion:
+            {
+                finished in
+                self.toView.removeFromSuperview()
+            }
+        )
     }
 }
