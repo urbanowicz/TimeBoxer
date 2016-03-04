98e34af2f973be57e2399548f3d0c782f698349d
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2016-03-04 11:29:05 +0100

    Moved FadeInAnimator from a private to a public class

--- TimeBoxer/TimerPausedViewController.swift
+++ TimeBoxer/TimerPausedViewController.swift
@@ -133,33 +133,4 @@ class TimerPausedViewController: UIViewController {
 
 
 
-//
-//MARK: FadeInAnimator
-//
-
-private class FadeInAnimator:NSObject, Animator {
-    let transitionDuration = 0.3
-    
-    func animateTransition(fromVC: UIViewController, toVC: UIViewController, container: UIView, completion: (() -> Void)?) {
-        //Remember: Container is the actual view of the parent controller.
-        //It already contains the fromVC.view.
-        //It is the animator's responsibility to remove the fromVC.view and insert the toVC.view
-        
-        
-        //Prepare the toVC.view to be faded in
-        toVC.view.alpha = 0.0
-        container.addSubview(toVC.view)
-        
-        UIView.animateWithDuration(transitionDuration,
-            animations: { toVC.view.alpha = 1.0 } ,
-            completion: {
-                finished in
-                fromVC.view.removeFromSuperview()
-                if let executeCompletionBlock = completion {
-                    executeCompletionBlock()
-                }
-            }
-        )
-    }
-}
 
