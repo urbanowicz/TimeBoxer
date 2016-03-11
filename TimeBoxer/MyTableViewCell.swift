85463aa7c03226c7ca0a7f81e2eca29e5eb6b636
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2016-03-11 18:37:32 +0100

    Stick with two possible font sizes for the project Name label 18.0 and 14.0

--- TimeBoxer/MyTableViewCell.swift
+++ TimeBoxer/MyTableViewCell.swift
@@ -28,20 +28,27 @@ class MyTableViewCell: UITableViewCell {
     func adjustFontSizeToFitTheFrame() {
         projectNameLabel.numberOfLines = 0
         projectNameLabel.lineBreakMode = NSLineBreakMode.ByTruncatingTail
-        var height = heightForLabel(projectNameLabel, maxWidth:projectNameLabel.frame.width)
-        var fontSize = projectNameLabel.font.pointSize
-        while height > projectNameLabel.frame.height && fontSize > 13.5 {
-            fontSize -= 0.5
-            projectNameLabel.font = projectNameLabel.font.fontWithSize(fontSize)
-            height = heightForLabel(projectNameLabel, maxWidth:projectNameLabel.frame.width)
+        var (height, numberOfLines) = heightAndNumberOfLinesForLabel(projectNameLabel, maxWidth:projectNameLabel.frame.width)
+        if numberOfLines >= 2 {
+            projectNameLabel.font = projectNameLabel.font.fontWithSize(14.0)
         }
+        
+//        var fontSize = projectNameLabel.font.pointSize
+//        while  height > projectNameLabel.frame.height && fontSize > 14 {
+//            fontSize -= 0.5
+//            projectNameLabel.font = projectNameLabel.font.fontWithSize(fontSize)
+//            (height, numberOfLines) = heightAndNumberOfLinesForLabel(projectNameLabel, maxWidth:projectNameLabel.frame.width)
+//        }
+
     }
     
-    private func heightForLabel(label:UILabel, maxWidth:CGFloat) -> CGFloat {
+    private func heightAndNumberOfLinesForLabel(label:UILabel, maxWidth:CGFloat) -> (CGFloat, Int) {
         let text = label.text! as NSString
         let attributes = [NSFontAttributeName: label.font]
         let frameSize = CGSizeMake(maxWidth, CGFloat.max)
         let labelSize = text.boundingRectWithSize(frameSize, options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: attributes, context: nil)
-        return labelSize.height
+        let height = labelSize.height
+        let numberOfLines = Int(ceil(height / label.font.lineHeight))
+        return (height:height, numberOfLines:numberOfLines)
     }
 }
