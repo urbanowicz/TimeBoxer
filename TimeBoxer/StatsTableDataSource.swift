efcf247ff51bc852e6533b706509ebb4aed383f4
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2016-03-22 10:24:19 +0100

    Working on stats view for the project #4

---
+++ TimeBoxer/StatsTableDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  StatsTableDataSource.swift
+//  TimeBoxer
+//
+//  Created by Tomasz Urbanowicz on 22/03/16.
+//  Copyright © 2016 Tomasz Urbanowicz. All rights reserved.
+//
+
+import UIKit
+
+class StatsTableDataSource: NSObject {
+
+}
--- TimeBoxer/StatsTableViewCell.swift
+++ TimeBoxer/StatsTableViewCell.swift
@@ -2,17 +2,46 @@
 //  StatsTableViewCell.swift
 //  TimeBoxer
 //
-//  Created by Tomasz on 21/03/16.
-//  Copyright © 2016 Tomasz. All rights reserved.
+//  Created by Tomasz Urbanowicz on 21/03/16.
+//  Copyright © 2016 Tomasz Urbanowicz. All rights reserved.
 //
 
 import UIKit
 
 class StatsTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var mondayLabel: UILabel!
+    @IBOutlet weak var mondayDateLabel: UILabel!
+    @IBOutlet weak var mondayBar: HorizontalBarView!
+    
+    @IBOutlet weak var tuesdayLabel: UILabel!
+    @IBOutlet weak var tuesdayDateLabel: UILabel!
+    @IBOutlet weak var tuesdayBar: HorizontalBarView!
+    
+    @IBOutlet weak var wednesdayLabel: UILabel!
+    @IBOutlet weak var wednesdayDateLabel: UILabel!
+    @IBOutlet weak var wednesdayBar: HorizontalBarView!
+    
+    @IBOutlet weak var thursdayLabel: UILabel!
+    @IBOutlet weak var thursdayDateLabel: UILabel!
+    @IBOutlet weak var thursdayBar: HorizontalBarView!
+
+    @IBOutlet weak var fridayLabel: UILabel!
+    @IBOutlet weak var fridayDateLabel: UILabel!
+    @IBOutlet weak var fridayBar: HorizontalBarView!
+    
+    
+    @IBOutlet weak var saturdayLabel: UILabel!
+    @IBOutlet weak var saturdayDateLabel: UILabel!
+    @IBOutlet weak var saturdayBar: HorizontalBarView!
+    
+    
+    @IBOutlet weak var sundayLabel: UILabel!
+    @IBOutlet weak var sundayDateLabel: UILabel!
+    @IBOutlet weak var sundayBar: HorizontalBarView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
     }
-
+    
 }
