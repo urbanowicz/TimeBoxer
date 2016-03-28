3f8035d9b8bdbec17666a76138738e39c8fc0e3c
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2016-03-28 10:38:14 +0200

    add dateToWeek func to NSCalendarExtension

--- TimeBoxer/NSCalendarExtension.swift
+++ TimeBoxer/NSCalendarExtension.swift
@@ -10,10 +10,20 @@ import UIKit
 
 extension NSCalendar {
     public func mondayBeforeTheDate(date:NSDate) -> NSDate {
-        let calendar = NSCalendar.currentCalendar()
-        let components = calendar.components([NSCalendarUnit.Weekday], fromDate:date)
+        let components = self.components([NSCalendarUnit.Weekday], fromDate:date)
         let numberOfDaysSinceMonday = components.weekday - 2
-        let monday = calendar.dateByAddingUnit(NSCalendarUnit.Day, value: -numberOfDaysSinceMonday, toDate: date, options: NSCalendarOptions.WrapComponents)!
+        let monday = self.dateByAddingUnit(NSCalendarUnit.Day, value: -numberOfDaysSinceMonday, toDate: date, options: NSCalendarOptions.WrapComponents)!
         return monday
+    }
+    
+    func dateToWeek(date:NSDate) -> [NSDate] {
+        let components = self.components([NSCalendarUnit.Weekday], fromDate:date)
+        let numberOfDaysSinceMonday = components.weekday - 2
+        let monday = self.dateByAddingUnit(NSCalendarUnit.Day, value: -numberOfDaysSinceMonday, toDate: date, options: NSCalendarOptions.WrapComponents)!
+        var week = [monday]
+        for i in 1 ... 6 {
+            week.append(self.dateByAddingUnit(NSCalendarUnit.Day, value: i, toDate: monday, options: NSCalendarOptions.WrapComponents)!)
+        }
+        return week
     }
 }
