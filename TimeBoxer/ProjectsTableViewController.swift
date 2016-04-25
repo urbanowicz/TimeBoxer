f6d034f3ee3f351a04435b2c6c048bed77628220
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2016-04-25 21:03:57 +0200

    Expanding Cell animation #1

--- TimeBoxer/ProjectsTableViewController.swift
+++ TimeBoxer/ProjectsTableViewController.swift
@@ -23,6 +23,9 @@ class ProjectsTableViewController: UIViewController, UITableViewDelegate {
     private var lastWorkedOnDateFormatter = LastWorkedOnDateFormatter()
     private var newProjectAdded:Bool = false
     
+    private var originalFrames = [CGRect]()
+    private var selectedCell: MyTableViewCell?
+    
     let transitionManager =
     TransitionManager(animator: ProjectsTableToAddProjectAnimator(),
         dismissAnimator:AddProjectToProjectsTableDismissAnimator())
@@ -30,6 +33,9 @@ class ProjectsTableViewController: UIViewController, UITableViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         //Setup UI elements
+        let longPressGestureRecognizer = UILongPressGestureRecognizer()
+        view.addGestureRecognizer(longPressGestureRecognizer)
+        longPressGestureRecognizer.addTarget(self, action: #selector(handleLongPressGesture(_:)))
         view.backgroundColor = Colors.almostBlack()
         setupAddProjectButton()
         setupProjectsTable()
@@ -126,6 +132,109 @@ class ProjectsTableViewController: UIViewController, UITableViewDelegate {
             return nil
         }
     }
+    
+    func handleLongPressGesture(gestureRecognizer:UILongPressGestureRecognizer) {
+
+        
+        if gestureRecognizer.state == .Began {
+            
+            let cell = self.cellAtPoint(gestureRecognizer.locationInView(self.projectsTableView))
+            self.selectedCell = cell
+            
+            //self.projectsTableView.backgroundColor = UIColor.yellowColor()
+            //cell?.facadeView.backgroundColor = UIColor.redColor()
+            func copyFrame(frame: CGRect) -> CGRect {
+                return CGRectMake(frame.origin.x, frame.origin.y, frame.width, frame.height)
+            }
+            
+            UIView.animateWithDuration(0.2, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut,
+                animations: {
+                    
+                    //1. Move the header up 
+                    self.originalFrames.append(copyFrame(self.timeBoxerLabel.frame))
+                    self.timeBoxerLabel.frame.origin.y = -1.0 * self.timeBoxerLabel.frame.height
+                    
+                    self.originalFrames.append(copyFrame(self.titleBarSeparator.frame))
+                    self.titleBarSeparator.frame.origin.y = -1.0 * self.titleBarSeparator.frame.height
+                    
+                    
+                    //2. Move the add project button down
+                    self.originalFrames.append(copyFrame(self.addProjectButton.frame))
+                    self.addProjectButton.frame.origin.y = self.view.frame.height
+                    
+                    //3. Make the project table fill the whole container view
+                    self.originalFrames.append(copyFrame(self.projectsTableView.frame))
+                    self.projectsTableView.frame = CGRect(x: 0.0, y: 0.0, width: self.view.frame.width, height: self.view.frame.height)
+                    
+                    
+                    //4. Move the visible cell out of the way
+                    for visibleCell in self.projectsTableView.visibleCells {
+                        if visibleCell.frame.origin.y < cell?.frame.origin.y {
+                            self.originalFrames.append(visibleCell.frame)
+                            visibleCell.frame.origin.y =  -1.0 * visibleCell.frame.height
+                            
+                        }
+                        
+                        if visibleCell.frame.origin.y > cell?.frame.origin.y {
+                            visibleCell.frame.origin.y = self.view.frame.height
+                            self.originalFrames.append(visibleCell.frame)
+                        }
+                    }
+
+                    //5. Expand the cell itself
+                    self.originalFrames.append(copyFrame(cell!.frame))
+                    cell?.frame = CGRect(x: 0, y: 0, width: self.projectsTableView.frame.width, height: self.projectsTableView.frame.height)
+                    
+                    cell?.facadeView.backgroundColor = Colors.purple()
+
+                    
+                },
+                completion: {
+                    finished in
+            })
+        }
+        
+        if gestureRecognizer.state == .Ended {
+            UIView.animateWithDuration(0.2, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut,
+                   animations: {
+                    
+                    //1. Restore the header
+                    self.timeBoxerLabel.frame = self.originalFrames[0]
+                    self.titleBarSeparator.frame = self.originalFrames[1]
+                    
+                    
+                    //2. Restore the project button
+                    self.addProjectButton.frame = self.originalFrames[2]
+                    
+                    //3. Make the project table fill the whole container view
+                    self.projectsTableView.frame = self.originalFrames[3]
+                    
+                    //4. Move the visible cell out of the way
+//                    for visibleCell in self.projectsTableView.visibleCells {
+//                        if visibleCell.frame.origin.y < cell?.frame.origin.y {
+//                            self.originalFrames.append(visibleCell.frame)
+//                            visibleCell.frame.origin.y =  -1.0 * visibleCell.frame.height
+//                            
+//                        }
+//                        
+//                        if visibleCell.frame.origin.y > cell?.frame.origin.y {
+//                            visibleCell.frame.origin.y = self.view.frame.height
+//                            self.originalFrames.append(visibleCell.frame)
+//                        }
+//                    }
+                    
+                    //5. Expand the cell itself
+                    self.selectedCell!.frame = self.originalFrames[4]
+                    self.selectedCell!.facadeView.backgroundColor = Colors.almostBlack()
+                    
+                    
+                },
+                   completion: {
+                    finished in
+            })
+            
+        }
+    }
 
 //MARK: Adjust font size
     func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
@@ -176,4 +285,5 @@ class ProjectsTableViewController: UIViewController, UITableViewDelegate {
     }
     
 }
+
 
