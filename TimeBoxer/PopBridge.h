c9ee6bff535c7c8f26bc5e024951e511646fb6a0
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2016-07-18 17:22:04 +0200

    Add Facebook POP framework

---
+++ TimeBoxer/PopBridge.h
@@ -0,0 +1,14 @@
+//
+//  PopBridge.h
+//  TimeBoxer
+//
+//  Created by Tomasz on 18/07/16.
+//  Copyright © 2016 Tomasz. All rights reserved.
+//
+
+#ifndef PopBridge_h
+#define PopBridge_h
+
+#import <POP/POP.h>
+
+#endif /* PopBridge_h */
